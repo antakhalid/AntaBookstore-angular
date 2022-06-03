ce0b0135c308237deb87a60c9a8d14fdb681b566
Khalid L. Anta <90225485+antakhalid@users.noreply.github.com>
2022-06-04 01:49:08 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'https://stunning-beignet-b07c62.netlify.app/';
 
 export const environment = {
   production: false,
